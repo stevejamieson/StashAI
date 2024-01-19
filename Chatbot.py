83add29564325f9342f31b3944730b5618de3919
Steve Jamieson <steve.jamieson@cgi.com>
2024-01-19 18:26:17 -0400

    updating bot language

--- Chatbot.py
+++ Chatbot.py
@@ -1,11 +1,4 @@
 
-
-# for speech-to-text
-import speech_recognition as sr
-
-# for text-to-speech
-from gtts import gTTS
-
 # for language model
 import transformers
 
@@ -23,36 +16,8 @@ class ChatBot():
     def __init__(self, name):
         print("----- Starting up", name, "-----")
         self.name = name
-
-    def speech_to_text(self):
-        recognizer = sr.Recognizer()
-        with sr.Microphone() as mic:
-            print("Listening...")
-            audio = recognizer.listen(mic)
-            self.text="ERROR"
-        try:
-            self.text = recognizer.recognize_google(audio)
-            print("Me  --> ", self.text)
-        except:
-            print("Me  -->  ERROR")
-            
-
+     
     @staticmethod
-    def text_to_speech(text):
-        print("Dev --> ", text)
-        speaker = gTTS(text=text, lang="en", slow=False)
-
-        speaker.save("res.mp3")
-        statbuf = os.stat("res.mp3")
-        mbytes = statbuf.st_size / 1024
-        duration = mbytes / 200
-        os.system('start res.mp3')  #if you are using mac->afplay or else for windows->start
-        # os.system("close res.mp3")
-        time.sleep(int(50*duration))
-        os.remove("res.mp3")
-        
-        
-
     def wake_up(self, text):
         return True if self.name in text.lower() else False
 
@@ -64,17 +29,15 @@ class ChatBot():
 # Running the AI
 if __name__ == "__main__":
     
-    ai = ChatBot(name="dev")
+    ai = ChatBot(name="StAshBot")
     nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
     os.environ["TOKENIZERS_PARALLELISM"] = "true"
     
     ex=True
     while ex:
-        ai.speech_to_text()
-
         ## wake up
         if ai.wake_up(ai.text) is True:
-            res = "Hello I am Dave the AI, what can I do for you?"
+            res = "Hello I am Stash the AI, what can I do for you?"
         
         ## action time
         elif "time" in ai.text:
@@ -82,20 +45,20 @@ if __name__ == "__main__":
         
         ## respond politely
         elif any(i in ai.text for i in ["thank","thanks"]):
-            res = np.random.choice(["you're welcome!","anytime!","no problem!","cool!","I'm here if you need me!","mention not"])
+            res = np.random.choice(["you're welcome!","anytime!","no problem!","cool!","I'm here if you need me!","forget about it"])
         
         elif any(i in ai.text for i in ["exit","close"]):
-            res = np.random.choice(["Tata","Have a good day","Bye","Goodbye","Hope to meet soon","peace out!"])
+            res = np.random.choice(["Tata","Have a good day","Buh Bye","Goodbye","Hope to see you again sometime soon","peace out!"])
             
             ex=False
         ## conversation
         else:   
             if ai.text=="ERROR":
-                res="Sorry, come again?"
+                res = np.random.choice(["Sorry, come again?","Dude, whats wrong?","That aint working","You gotta repeat that"])
+            
             else:
                 chat = nlp(transformers.Conversation(ai.text), pad_token_id=50256)
                 res = str(chat)
                 res = res[res.find("bot >> ")+6:].strip()
 
-        ai.text_to_speech(res)
-    print("----- Closing down Dev -----")
+    print("----- Closing down StAshBot -----")
